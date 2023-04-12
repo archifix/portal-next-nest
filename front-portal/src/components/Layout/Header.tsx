733e4dbc8b103fcf09b5f988447ebf6fe34446f4
Илья <107472693+archifix@users.noreply.github.com>
2023-04-12 22:19:52 +0300

    New

--- front-portal/src/components/Layout/Header.tsx
+++ front-portal/src/components/Layout/Header.tsx
@@ -1,12 +1,13 @@
 import React from "react"
 import Link from "next/link"
+import Image from "next/image"
 import styles from "@/styles/Header.module.scss"
 
 const Header = () => {
 	return (
 		<div className={styles.header}>
 			<Link href={"/"}>
-				<img src="/yuo.png" alt="logo" className={styles.logo} />
+				<Image src="/yuo.png" alt="Landscape picture" width={84} height={130} />
 			</Link>
 			<div className={styles.title}>
 				<h1 className="">южный округ</h1>
