0889e495f3856c1ab1ff64943051f6f91cd46f6d
Илья <107472693+archifix@users.noreply.github.com>
2023-04-13 17:35:30 +0300

    New

--- front-portal/src/components/Layout/Header.tsx
+++ front-portal/src/components/Layout/Header.tsx
@@ -5,15 +5,44 @@ import styles from "@/styles/Header.module.scss"
 
 const Header = () => {
 	return (
-		<div className={styles.header}>
-			<Link href={"/"}>
-				<Image src="/yuo.png" alt="Landscape picture" width={84} height={130} />
-			</Link>
-			<div className={styles.title}>
-				<h1 className="">южный округ</h1>
-				<h2>войск национальной гвардии</h2>
+		<>
+			<div className={styles.header}>
+				<Link href={"/"}>
+					<Image
+						src="/yuo.png"
+						alt="Landscape picture"
+						width={84}
+						height={130}
+					/>
+				</Link>
+				<div className={styles.title}>
+					<h1>южный округ</h1>
+					<h2>войск национальной гвардии</h2>
+				</div>
 			</div>
-		</div>
+			<nav className={styles.navigation}>
+				<ul className={styles.navigation__ul}>
+					<li className={styles.navigation__ul__item}>
+						<Link href={"/"}>Главная</Link>
+					</li>
+					<li className={styles.navigation__ul__item}>
+						<Link href={"/"}>Главная</Link>
+					</li>
+					<li className={styles.navigation__ul__item}>
+						<Link href={"/"}>Главная</Link>
+					</li>
+					<li className={styles.navigation__ul__item}>
+						<Link href={"/"}>Главная</Link>
+					</li>
+					<li className={styles.navigation__ul__item}>
+						<Link href={"/"}>Главная</Link>
+					</li>
+				</ul>
+				<Link href={"/"} className="justify-end">
+					Login
+				</Link>
+			</nav>
+		</>
 	)
 }
 
